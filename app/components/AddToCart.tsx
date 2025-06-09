a3f20cf752afef86d4964fe0f9cd6e8f5633e0c4
gobo-larry <wrgamejuter@gmail.com>
2025-06-10 00:24:41 +0100

    next.js fundamentals

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button onClick={()=> console.log('Button clicked!')}>
+        Click Me
+      </button>
+    </div>
+  )
+}
+
+export default AddToCart
