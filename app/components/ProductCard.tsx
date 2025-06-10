6bc27d08d71f9e756f75de3c017fa0f2f69bc6fb
gobo-larry <wrgamejuter@gmail.com>
2025-06-10 20:03:49 +0100

    tailwinds

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,12 +1,15 @@
 
 import React from 'react'
 import AddToCart from './AddToCart'
-import styles from './ProductCard.module.css' 
+
 
 const ProductCard = () => {
   
   return (
-    <div className={styles.card}>
+     //<div className={styles.card}> using CSS module for styling
+    //tailwindcss for styling be1low
+    <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-600'>
+   
       <AddToCart />
       
     </div>
