12fcbad8ac912b45aa4e66b23df37270455a2063
gobo-larry <wrgamejuter@gmail.com>
2025-06-13 00:28:40 +0100

    Loading uis

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import UserTable from './UserTable'
 import Link from 'next/link'
 
@@ -19,8 +19,10 @@ const usersPage = async ({ searchParams:{ sortOrder} }: Props) => {
     
     <h1>Users</h1>
     <Link href='/users/new' className='btn'>New User</Link>
-    
+    <Suspense fallback={<p>Loading...</p>}>
     <UserTable sortOrder={sortOrder}/>
+
+    </Suspense>
       
     
     
