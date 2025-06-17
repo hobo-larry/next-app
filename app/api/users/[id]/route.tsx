0bb4f4e68a49cea9410e29790312890803801755
gobo-larry <wrgamejuter@gmail.com>
2025-06-17 15:26:34 +0100

    updating object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -15,3 +15,27 @@ export function GET(request:NextRequest, {params}:{params: {id: number }}){
     }
    
 }
+
+export async function PUT(request:NextRequest, {params}:{params: {id: number }}){
+  //validate the request body
+  //if invalid, return 400
+  //fetch the user with the given id
+  //if doesnt exist
+  // error  404
+  //else update user
+  //return updated user
+  const body =  await request.json()
+  if(!body.name){
+    return NextResponse.json({error:'invalid name'}, {status:400})
+  }
+  if(params.id >10 ){
+    return NextResponse.json({error:'user not found'}, {status:404})
+
+
+  }else{
+    return NextResponse.json({id:1, name:body.name})
+  }
+  
+}
+
+
