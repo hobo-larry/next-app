3faf231c4349d690e30708f36fe48ffbbb5f5ddd
gobo-larry <wrgamejuter@gmail.com>
2025-06-24 14:00:17 +0100

    creating new user

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -14,11 +14,25 @@ export async function GET(request:NextRequest){
 }
 
 export async function POST(request:NextRequest){
+  
   const body = await request.json()
   const validation = schema.safeParse(body)
+  const user = await prisma.user.findUnique({
+    where: { email: body.email
+    }})
+    if(user){
+      return NextResponse.json({error: "User already exists"}, {status: 400})
+    }
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+    
   if(!validation.success){
     return NextResponse.json(validation.error.errors, {status:400})
   }
-  return NextResponse.json({id: 1, name: body.name},{status:201})
+  return NextResponse.json(newUser,{status:201})
 }
 
