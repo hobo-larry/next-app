2ce5561b1f468aae8a052087d89589807ce9232d
gobo-larry <wrgamejuter@gmail.com>
2025-07-09 23:51:46 +0100

    fixed profile drop menu with image and without

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,9 +3,11 @@ import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 import React from 'react'
 
+
 const NavBar = () => {
-  const {status, data: session} = useSession()
-  
+  const { status, data: session } = useSession();
+  console.log("Session image URL:", session?.user?.image);
+
   return (
     <div className="flex bg-slate-200 p-3 space-x-3 items-center">
       <Link href="/" className="mr-5">
@@ -15,21 +17,24 @@ const NavBar = () => {
       {status === "loading" && <div>Loading...</div>}
       {status === "authenticated" && (
         <div>
-          <Link href="/users">Users</Link>
-          {session.user!.image && (
+          {(session.user!.image || session.user!.name) && (
             <div className="dropdown">
               <div
                 tabIndex={0}
                 role="button"
                 className="btn btn-ghost btn-circle avatar"
               >
-                <img
-                  src={session.user!.image}
-                  alt="user image"
-                  className="w-10 h-10 rounded-full inline-block ml-2"
-                  onClick={() => console.log("User image clicked")}
-                />
+                {session.user!.image ? (
+                  <img
+                    src={session.user!.image}
+                    alt="user image"
+                    className="w-9 h-9 rounded-full inline-block ml-2"
+                  />
+                ) : (
+                  <span>{session.user!.name?.toLocaleUpperCase()}</span>
+                )}
               </div>
+
               <ul
                 tabIndex={0}
                 className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
@@ -40,19 +45,20 @@ const NavBar = () => {
                   </Link>
                 </li>
                 <li>
-                  <a>Item 2</a>
+                  <Link href="/upload">Upload</Link>
+                </li>
+                <li>
+                  <Link href="/users">Users</Link>
+                </li>
+                <li>
+                  <Link href="/api/auth/signout">Logout</Link>
                 </li>
               </ul>
             </div>
           )}
-          <Link href="/upload" className="ml-3">
-            Upload
-          </Link>
-          <Link href="/api/auth/signout" className="ml-3">
-            Logout
-          </Link>
         </div>
       )}
+
       {status === "unauthenticated" && (
         <div>
           <Link href="/api/auth/signin" className="ml-3">
@@ -65,6 +71,6 @@ const NavBar = () => {
       )}
     </div>
   );
-}
+};
 
 export default NavBar
