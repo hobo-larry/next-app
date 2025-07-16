022b1dac65010459882096a860f95a6fdfd1208e
gobo-larry <wrgamejuter@gmail.com>
2025-07-16 16:27:40 +0100

    search engine optimization aka SEO

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Link from 'next/link'
-import ProductCard from './components/ProductCard'
-import { getServerSession } from 'next-auth'
-import { authOptions } from './api/auth/[...nextauth]/route'
+import Link from "next/link";
+import ProductCard from "./components/ProductCard";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 import Image from "next/image";
 import coffee from "@/public/images/coffee.jpg";
+import { Metadata } from "next";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -23,4 +24,13 @@ export default async function Home() {
       {/* this is how to use remote images in next.js for this to work u need to register it in the next config file*/}
     </main>
   );
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const product = await fetch("..");
+  return {
+    title: "product.title without the quotes ahah, this is an example",
+    description:
+      "product.description without the quotes ahah, this is an example",
+  };
 }
